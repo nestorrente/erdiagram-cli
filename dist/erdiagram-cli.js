4cbedf08e0ab16f3cf19728ad975774dfff28588
Néstor Pérez González <nperez@continuumsecurity.net>
2021-02-08 19:18:23 +0100

    Updated erdiagram dependency.

--- dist/erdiagram-cli.js
+++ dist/erdiagram-cli.js
@@ -4,7 +4,7 @@
  * 
  * Released under the MIT License.
  * 
- * Build date: 2021-02-08T18:10:46.428Z
+ * Build date: 2021-02-08T18:18:10.559Z
  */!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=22)}([function(e,t){e.exports=require("path")},function(e,t,r){
 /*!
  * Entity-Relationship Diagram Code Generator v0.1.0
@@ -12,7 +12,7 @@
  * 
  * Released under the MIT License.
  * 
- * Build date: 2021-02-08T18:09:50.988Z
+ * Build date: 2021-02-08T18:17:28.628Z
  */
 var n;"undefined"!=typeof self&&self,n=function(){return function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s="./src/main/module-entry.ts")}({"./node_modules/pluralize/pluralize.js":
 /*!*********************************************!*\
@@ -22,7 +22,7 @@ var n;"undefined"!=typeof self&&self,n=function(){return function(e){var t={};fu
 /*!****************************************************************************!*\
   !*** ./src/main/erdiagram/common/config/AbstractComponentConfigManager.ts ***!
   \****************************************************************************/
-/*! exports provided: default */function(e,t,r){"use strict";r.r(t);var n=function(){function e(){}return e.prototype.mergeWithDefaultConfig=function(e){return this.mergeConfigs(this.getDefaultConfig(),e)},e}();t.default=n},"./src/main/erdiagram/common/config/exports.ts":
+/*! exports provided: default */function(e,t,r){"use strict";r.r(t);var n=function(){function e(){}return e.prototype.mergeWithDefaultConfig=function(e){return this.mergeConfigs(this.getDefaultConfig(),e)},e.prototype.cloneConfig=function(e){return this.mergeConfigs(e)},e}();t.default=n},"./src/main/erdiagram/common/config/exports.ts":
 /*!*****************************************************!*\
   !*** ./src/main/erdiagram/common/config/exports.ts ***!
   \*****************************************************/
