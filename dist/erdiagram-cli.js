75189080eb13b230b86e231113dc36eb3a8b434e
nestorrente <nestorrente@gmail.com>
2021-02-23 22:31:53 +0100

    Changed version number to "0.1.0-alpha.0".

--- dist/erdiagram-cli.js
+++ dist/erdiagram-cli.js
@@ -1,10 +1,10 @@
 /*!
- * Entity-Relationship Diagram Code Generator v0.1.0
+ * Entity-Relationship Diagram Code Generator v0.1.0-alpha.0
  * https://github.com/nestorrente/erdiagram-cli
  * 
  * Released under the MIT License.
  * 
- * Build date: 2021-02-23T21:24:22.053Z
+ * Build date: 2021-02-23T21:29:09.095Z
  */!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=22)}([function(e,t,r){
 /*!
  * Entity-Relationship Diagram Code Generator v0.1.0-alpha.0
