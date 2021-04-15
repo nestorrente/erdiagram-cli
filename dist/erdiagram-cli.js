eba422bd29e373109760dc64643e392189f2616b
Néstor Pérez González <nperez@continuumsecurity.net>
2021-04-15 15:56:17 +0200

    Updated some dependencies. Updated build.

--- dist/erdiagram-cli.js
+++ dist/erdiagram-cli.js
@@ -5,7 +5,7 @@
  *
  * Released under the MIT License.
  *
- * Build date: 2021-04-15T13:48:12.040Z
+ * Build date: 2021-04-15T13:55:11.025Z
  */!function(e){var t={};function r(a){if(t[a])return t[a].exports;var n=t[a]={i:a,l:!1,exports:{}};return e[a].call(n.exports,n,n.exports,r),n.l=!0,n.exports}r.m=e,r.c=t,r.d=function(e,t,a){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:a})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var a=Object.create(null);if(r.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)r.d(a,n,function(t){return e[t]}.bind(null,n));return a},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=58)}([function(e,t,r){
 /*!
  * Entity-Relationship Diagram Code Generator v1.0.0-beta.3
